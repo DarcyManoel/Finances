2bd76747ff4b298737fbbcb896aac2c2f8808093
DarcyManoel <darcman00@gmail.com>
2024-04-20 21:37:35 +0930

    Calculate sum of transfers for accounts and counterparties

--- scriptsheet.js
+++ scriptsheet.js
@@ -1,16 +1,39 @@
 function renderOverview(){
 	for(i1=0;i1<loans.length;i1++){
-		document.getElementById(`loans`).innerHTML+=`<details name="counterparty"><summary>`+loans[i1].counterparty+`</summary><div id="`+loans[i1].counterparty+` Accounts" class="accounts"></div></summary>`
+		var accountsSum=0
+		var transfersSum=0
+		var colour
 		for(i2=0;i2<loans[i1].accounts.length;i2++){
-			document.getElementById(loans[i1].counterparty+` Accounts`).innerHTML+=`<details name="account"><summary>`+loans[i1].accounts[i2].title+`</summary><table id="`+loans[i1].counterparty+` `+loans[i1].accounts[i2].title+` Transfers" class="transfers"></table></details>`
+			transfersSum=0
 			for(i3=0;i3<loans[i1].accounts[i2].transfers.length;i3++){
-				var colour
-				if((loans[i1].accounts[i2].transfers[i3][1]/Math.abs(loans[i1].accounts[i2].transfers[i3][1]))>0){
+				transfersSum=transfersSum+loans[i1].accounts[i2].transfers[i3][1]
+			}
+			accountsSum=accountsSum+transfersSum
+		}
+		if(accountsSum>=0){
+			colour=`green`
+		}else{
+			colour=`red`
+		}
+		document.getElementById(`loans`).innerHTML+=`<details name="counterparty"><summary>`+loans[i1].counterparty+` <span class="`+colour+`">`+accountsSum.toFixed(2)+`</span></summary><div id="`+loans[i1].counterparty+` Accounts" class="accounts"></div></summary>`
+		for(i2=0;i2<loans[i1].accounts.length;i2++){
+			transfersSum=0
+			for(i3=0;i3<loans[i1].accounts[i2].transfers.length;i3++){
+				transfersSum=transfersSum+loans[i1].accounts[i2].transfers[i3][1]
+			}
+			if(transfersSum>=0){
+				colour=`green`
+			}else{
+				colour=`red`
+			}
+			document.getElementById(loans[i1].counterparty+` Accounts`).innerHTML+=`<details name="account"><summary>`+loans[i1].accounts[i2].title+` <span class="`+colour+`">`+transfersSum.toFixed(2)+`</span></summary><table id="`+loans[i1].counterparty+` `+loans[i1].accounts[i2].title+` Transfers" class="transfers"></table></details>`
+			for(i3=0;i3<loans[i1].accounts[i2].transfers.length;i3++){
+				if((loans[i1].accounts[i2].transfers[i3][1]/Math.abs(loans[i1].accounts[i2].transfers[i3][1]))>=0){
 					colour=`green`
 				}else{
 					colour=`red`
 				}
-				document.getElementById(loans[i1].counterparty+` `+loans[i1].accounts[i2].title+` Transfers`).innerHTML+=`<tr><td>`+loans[i1].accounts[i2].transfers[i3][0].join(`-`)+`</td><td class="`+colour+`">`+(Math.round(loans[i1].accounts[i2].transfers[i3][1]*100)/100).toFixed(2)+`</td></tr>`
+				document.getElementById(loans[i1].counterparty+` `+loans[i1].accounts[i2].title+` Transfers`).innerHTML+=`<tr><td>`+loans[i1].accounts[i2].transfers[i3][0].join(`-`)+`</td><td class="`+colour+`">`+loans[i1].accounts[i2].transfers[i3][1].toFixed(2)+`</td></tr>`
 			}
 		}
 	}
