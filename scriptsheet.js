3edc542f61a195c99781ed9d4ccdb72736c75e08
DarcyManoel <darcman00@gmail.com>
2024-04-20 22:29:52 +0930

    Show days between transfers, improve outstanding balance styling

--- scriptsheet.js
+++ scriptsheet.js
@@ -2,6 +2,7 @@ function renderOverview(){
 	for(i1=0;i1<loans.length;i1++){
 		var accountsSum=0
 		var transfersSum=0
+		var state=`open`
 		var colour
 		for(i2=0;i2<loans[i1].accounts.length;i2++){
 			transfersSum=0
@@ -10,30 +11,42 @@ function renderOverview(){
 			}
 			accountsSum=accountsSum+transfersSum
 		}
-		if(accountsSum>=0){
+		if(accountsSum==0){
+			state=`closed`
+			colour=`hidden`
+		}else if(accountsSum>0){
 			colour=`green`
 		}else{
 			colour=`red`
 		}
-		document.getElementById(`loans`).innerHTML+=`<details name="counterparty"><summary>`+loans[i1].counterparty+` <span class="`+colour+`">`+accountsSum.toFixed(2)+`</span></summary><div id="`+loans[i1].counterparty+` Accounts" class="accounts"></div></summary>`
+		document.getElementById(`loans`).innerHTML+=`<details name="counterparty"><summary class="`+state+`">`+loans[i1].counterparty+` <span class="`+colour+`">`+accountsSum.toFixed(2)+`</span></summary><div id="`+loans[i1].counterparty+` Accounts" class="accounts"></div></summary>`
 		for(i2=0;i2<loans[i1].accounts.length;i2++){
 			transfersSum=0
 			for(i3=0;i3<loans[i1].accounts[i2].transfers.length;i3++){
 				transfersSum=transfersSum+loans[i1].accounts[i2].transfers[i3][1]
 			}
-			if(transfersSum>=0){
+			if(transfersSum==0){
+				state=`closed`
+				colour=`hidden`
+			}else if(transfersSum>0){
 				colour=`green`
 			}else{
 				colour=`red`
 			}
-			document.getElementById(loans[i1].counterparty+` Accounts`).innerHTML+=`<details name="account"><summary>`+loans[i1].accounts[i2].title+` <span class="`+colour+`">`+transfersSum.toFixed(2)+`</span></summary><table id="`+loans[i1].counterparty+` `+loans[i1].accounts[i2].title+` Transfers" class="transfers"></table></details>`
+			document.getElementById(loans[i1].counterparty+` Accounts`).innerHTML+=`<details name="account"><summary class="`+state+`">`+loans[i1].accounts[i2].title+` <span class="`+colour+`">`+transfersSum.toFixed(2)+`</span></summary><table class="`+state+`" id="`+loans[i1].counterparty+` `+loans[i1].accounts[i2].title+` Transfers" class="transfers"></table></details>`
 			for(i3=0;i3<loans[i1].accounts[i2].transfers.length;i3++){
-				if((loans[i1].accounts[i2].transfers[i3][1]/Math.abs(loans[i1].accounts[i2].transfers[i3][1]))>=0){
+				if(loans[i1].accounts[i2].transfers[i3][1]>0){
 					colour=`green`
 				}else{
 					colour=`red`
 				}
-				document.getElementById(loans[i1].counterparty+` `+loans[i1].accounts[i2].title+` Transfers`).innerHTML+=`<tr><td>`+loans[i1].accounts[i2].transfers[i3][0].join(`-`)+`</td><td class="`+colour+`">`+loans[i1].accounts[i2].transfers[i3][1].toFixed(2)+`</td></tr>`
+				var dayDiff=``
+				if(loans[i1].accounts[i2].transfers[i3-1]){
+					date1=new Date(loans[i1].accounts[i2].transfers[i3-1][0][1]+`/`+loans[i1].accounts[i2].transfers[i3-1][0][0]+`/`+loans[i1].accounts[i2].transfers[i3-1][0][2])
+					date2=new Date(loans[i1].accounts[i2].transfers[i3][0][1]+`/`+loans[i1].accounts[i2].transfers[i3][0][0]+`/`+loans[i1].accounts[i2].transfers[i3][0][2])
+					dayDiff=Math.round((date2.getTime()-date1.getTime())/(1000*3600*24))+` days`
+				}
+				document.getElementById(loans[i1].counterparty+` `+loans[i1].accounts[i2].title+` Transfers`).innerHTML+=`<tr><td>`+loans[i1].accounts[i2].transfers[i3][0].join(`-`)+`</td><td class="`+colour+` detail">`+loans[i1].accounts[i2].transfers[i3][1].toFixed(2)+`</td><td class="detail">`+dayDiff+`</td></tr>`
 			}
 		}
 	}
